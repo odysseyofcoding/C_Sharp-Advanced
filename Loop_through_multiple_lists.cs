07dacb64432fccaead5bb6cb3e7512020c8e4895
odysseyofcoding <74965926+odysseyofcoding@users.noreply.github.com>
2021-02-10 16:07:14 +0100

    Update Loop_through_multiple_lists.cs

--- Loop_through_multiple_lists.cs
+++ Loop_through_multiple_lists.cs
@@ -1,1 +1,24 @@
 using System;
+using System.Linq;
+
+namespace GitHub_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] { "Hello", "Test", "bye" };
+            int[] numbers = new int[] { 1, 2, 3 };
+            char[] signs = new char[] {'?','$','!' };
+
+            var forged = names.Zip(numbers, (n, e) => new { n, e }).Zip(signs, (x, s) => new { Names = x.n, Numbers = x.e, Signs = s });
+            foreach (var item in forged)
+            {
+             
+                Console.WriteLine(string.Join(", ", item.Names, item.Numbers, item.Signs));
+            }
+
+
+        }
+    }
+}
